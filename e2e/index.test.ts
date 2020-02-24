52dfb9e8b37475068a1fb339da4d977310777184
amir-arad <amir@orbs.com>
2020-02-24 18:06:34 +0200

    use docker to poll E2E state

--- e2e/index.test.ts
+++ e2e/index.test.ts
@@ -3,6 +3,9 @@ import { dockerComposeTool, getAddressForService } from 'docker-compose-mocha';
 import fetch from 'node-fetch';
 import { join } from 'path';
 import { retry } from 'ts-retry-promise';
+import { exec as cb_exec } from 'child_process';
+import * as util from 'util';
+const exec = util.promisify(cb_exec);
 const pathToCompose = join(__dirname, 'docker-compose.yml');
 
 const envName = dockerComposeTool(
@@ -10,27 +13,38 @@ const envName = dockerComposeTool(
     test.serial.after.always.bind(test.serial.after),
     pathToCompose,
     {
-        shouldPullImages: false,
-        healthCheck: {
-            state: true,
-            options: {
-                custom: {
-                    app: (addr: string) =>
-                        fetch('http://' + addr).then(
-                            _ => true,
-                            _ => false
-                        )
-                }
-            }
-        }
+        shouldPullImages: false
     }
 );
 
-test('happy flow', async t => {
+async function serviceFetch(envName: string, pathToCompose: string) {
+    const containerName = (
+        await exec(`docker-compose  -p ${envName} -f "${pathToCompose}" ps app | awk '{print $1}' | tail -n 1`)
+    ).stdout.trim();
+    console.log('containerName', containerName);
+    const resStr = (
+        await exec(
+            `docker run --network container:${containerName} appropriate/curl  -s --retry 10 --retry-delay 1 --retry-connrefused http://localhost:80`
+        )
+    ).stdout;
+    console.log('resStr', resStr);
+    const res = JSON.parse(resStr);
+    return res;
+}
+
+async function localFetch(envName: string, pathToCompose: string) {
     const addr = await getAddressForService(envName, pathToCompose, 'app', 80);
-
     console.log('addr', addr);
+    const res = await (await retry(() => fetch('http://' + addr), { retries: 10, delay: 300 })).json();
+    return res;
+}
 
-    const res = await retry(() => fetch('http://' + addr), { retries: 10, delay: 300 });
-    t.deepEqual(await res.json(), { message: 'hello world' });
+test('happy flow', async t => {
+    try {
+        const res = await serviceFetch(envName, pathToCompose); // localFetch(envName, pathToCompose)
+        t.deepEqual(res, { message: 'hello world' });
+    } catch (err) {
+        console.error(err);
+        throw err;
+    }
 });
