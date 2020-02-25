43ec66abc73c0f17152d113339d62b302c31131e
amir-arad <amir@orbs.com>
2020-02-25 14:22:59 +0200

    extract e2e test driver

--- e2e/index.test.ts
+++ e2e/index.test.ts
@@ -1,52 +1,13 @@
 import test from 'ava';
-import { dockerComposeTool, getAddressForService } from 'docker-compose-mocha';
-import fetch from 'node-fetch';
 import { join } from 'path';
-import { retry } from 'ts-retry-promise';
-import { exec as cb_exec } from 'child_process';
-import * as util from 'util';
-const exec = util.promisify(cb_exec);
+import { TestEnvironment } from './driver';
 const pathToCompose = join(__dirname, 'docker-compose.yml');
 
-const envName = dockerComposeTool(
-    test.serial.before.bind(test.serial),
-    test.serial.after.always.bind(test.serial.after),
-    pathToCompose,
-    {
-        shouldPullImages: false
-    }
-);
+const env = new TestEnvironment(pathToCompose);
 
-async function serviceFetch(envName: string, pathToCompose: string) {
-    console.log((await exec(`docker-compose  -p ${envName} -f "${pathToCompose}" ps app`)).stdout);
-    const containerName = `${envName}_app_1`;
-    // (
-    //     await exec(`docker-compose  -p ${envName} -f "${pathToCompose}" ps app | awk '{print $1}' | tail -n 1`)
-    // ).stdout.trim();
-    console.log('containerName', containerName);
-    const resStr = (
-        await exec(
-            `docker run --network container:${containerName} appropriate/curl  -s --retry 10 --retry-delay 1 --retry-connrefused http://localhost:80`
-        )
-    ).stdout;
-    console.log('resStr', resStr);
-    const res = JSON.parse(resStr);
-    return res;
-}
+env.init();
 
-async function localFetch(envName: string, pathToCompose: string) {
-    const addr = await getAddressForService(envName, pathToCompose, 'app', 80);
-    console.log('addr', addr);
-    const res = await (await retry(() => fetch('http://' + addr), { retries: 10, delay: 300 })).json();
-    return res;
-}
-
-test('happy flow', async t => {
-    try {
-        const res = await serviceFetch(envName, pathToCompose); // localFetch(envName, pathToCompose)
-        t.deepEqual(res, { message: 'hello world' });
-    } catch (err) {
-        console.error(err);
-        throw err;
-    }
+test.serial('happy flow', async t => {
+    const res = await env.fetch('app');
+    t.deepEqual(res, { message: 'hello world' });
 });
