19465f813ec59ad6676b9263b2be35206d7a650b
Kirill Maksimov <kirill@orbs.com>
2020-02-27 12:57:24 +0200

    Service stub

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,22 @@
-import { createServer } from 'http';
-import stub from './stub.json';
+import { createServer, ServerResponse } from 'http';
+import fetch from "node-fetch";
 
-export function serve(port: number) {
+export interface Config {
+    ethereumEndpoint: string;
+    configUrl: string;
+}
+
+async function proxy(config: Config, response: ServerResponse) {
+    const res = await fetch(config.configUrl);
+    const body = await res.text();
+
+    response.writeHead(200, { 'Content-Type': 'application/json' });
+    response.end(body);
+}
+
+export function serve(port: number, config: Config) {
     const server = createServer((_request, response) => {
-        response.writeHead(200, { 'Content-Type': 'application/json' });
-        response.end(JSON.stringify(stub));
+        proxy(config, response).catch(console.error);
     });
     server.listen(port);
     console.log('Server starting..');
--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,17 @@
-import { serve } from '.';
+import { serve, Config } from '.';
+import { readFileSync } from 'fs';
+import yargs from 'yargs';
 
-serve(80);
+const options = yargs.option('config', {
+    type: 'array',
+    description: 'list of config files'
+}).argv;
+
+console.log(options.config)
+const config: Config = options.config?.reduce((result: any, configFile: any) => {
+    return Object.assign(result, JSON.parse(readFileSync(configFile).toString()));
+}, {}) as Config;
+
+console.log(config)
+
+serve(7666, config);
