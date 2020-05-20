9787a3723d9af5e3ef8ada9c2759072521de2221
amir-arad <amir@orbs.com>
2020-05-20 12:01:25 +0300

    change implementation of gossip ports

--- src/ports.ts
+++ src/ports.ts
@@ -1,16 +1,6 @@
-export const GOSSIP_PORT_BEGIN = 4000;
-export const GOSSIP_PORT_RANGE = 1000;
+export const GOSSIP_PORT_BASE = 32768;
+export const BASE = 1000000;
 
 export function getVirtualChainPort(id: string) {
-    return GOSSIP_PORT_BEGIN + (stringHash(id) % GOSSIP_PORT_RANGE);
-}
-
-export function stringHash(id: string) {
-    let result = 0;
-    for (let i = 0; i < id.length; i++) {
-        const chr = id.charCodeAt(i);
-        result = (result << 5) - result + chr;
-        result |= 0; // Convert to 32bit integer
-    }
-    return result;
+    return GOSSIP_PORT_BASE + Number(id) - BASE;
 }
