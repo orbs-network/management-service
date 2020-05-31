0e0b8a438152ce036815e6e5a8811bc8ef67e1b1
Kirill Maksimov <kirill@orbs.com>
2020-05-31 13:27:02 +0300

    Remove ECR

--- .circleci/release.sh
+++ .circleci/release.sh
@@ -7,7 +7,6 @@ else
   VERSION=experimental
 fi
 
-$(aws ecr get-login --no-include-email --region us-west-2)
 docker login -u $DOCKER_HUB_LOGIN -p $DOCKER_HUB_PASSWORD
 
 docker push orbsnetwork/management-service:$VERSION
