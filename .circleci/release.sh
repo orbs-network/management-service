5ecb92b986709a9e3d4de11b99667923194058ab
Kirill Maksimov <kirill@orbs.com>
2020-05-31 13:51:45 +0300

    Fix tags

--- .circleci/release.sh
+++ .circleci/release.sh
@@ -9,4 +9,5 @@ fi
 
 docker login -u $DOCKER_HUB_LOGIN -p $DOCKER_HUB_PASSWORD
 
+docker tag orbsnetwork/management-service:$(cat .version) orbsnetwork/management-service:$VERSION
 docker push orbsnetwork/management-service:$VERSION
