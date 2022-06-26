507dd7bc9b15cdc8c6b1ef9f48b7ab05b84bcbd9
uvorbs <yuval@orbs.com>
2022-06-26 15:25:37 +0300

    fix election contract again

---
+++ yuval.sh
@@ -0,0 +1,27 @@
+docker pull orbsnetworkstaging/management-service:v2.1.2-0a399120
+docker tag  orbsnetworkstaging/management-service:v2.1.2-0a399120 orbsnetworkstaging/management-service:v100.5.0-immediate
+docker push orbsnetworkstaging/management-service:v100.5.0-immediate
+
+docker pull orbsnetworkstaging/management-service:v2.1.2-a0916e34
+docker tag  orbsnetworkstaging/management-service:v2.1.2-a0916e34 orbsnetworkstaging/management-service:v100.5.1-immediate
+docker push orbsnetworkstaging/management-service:v100.5.1-immediate
+
+docker pull kornkitti/express-hello-world
+docker tag  kornkitti/express-hello-world orbsnetworkstaging/express-hello-world:v1.0.0-immediate
+docker push orbsnetworkstaging/express-hello-world:v1.0.0-immediate
+
+#### deddy
+
+docker pull orbsnetworkstaging/management-service:v2.1.2-37f0dae5
+docker tag  orbsnetworkstaging/management-service:v2.1.2-37f0dae5 orbsnetworkstaging/management-service:v100.5.0-immediate
+docker push orbsnetworkstaging/management-service:v100.5.0-immediate
+
+# odnp + keepers
+docker pull orbsnetworkstaging/management-service:v2.1.2-6dbcab18
+docker tag  orbsnetworkstaging/management-service:v2.1.2-6dbcab18 orbsnetworkstaging/management-service:v100.5.1-immediate 
+docker push orbsnetworkstaging/management-service:v100.5.1-immediate 
+
+# merge and rename
+docker pull orbsnetworkstaging/management-service:v2.1.2-eae055a5
+docker tag  orbsnetworkstaging/management-service:v2.1.2-eae055a5 orbsnetworkstaging/management-service:v100.6.0-immediate
+docker push orbsnetworkstaging/management-service:v100.6.0-immediate
