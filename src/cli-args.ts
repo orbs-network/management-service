d751eae20e62ebe3de0f2c070cad36f0d38049ca
Luke Rogerson <luke@orbs.com>
2023-07-26 10:19:43 +0100

    Support passing required config values via environment variables

--- src/cli-args.ts
+++ src/cli-args.ts
@@ -1,4 +1,4 @@
-import { ServiceConfiguration, validateServiceConfiguration, defaultServiceConfiguration } from './config';
+import { ServiceConfiguration, defaultServiceConfiguration, validateServiceConfiguration } from './config';
 import { readFileSync } from 'fs';
 import yargs from 'yargs';
 import * as Logger from './logger';
@@ -7,19 +7,23 @@ export function parseArgs(argv: string[]): ServiceConfiguration {
   const options = yargs(argv)
     .option('config', {
       type: 'array',
-      required: true,
       string: true,
       description: 'list of config files',
     })
     .exitProcess(false)
     .parse();
 
+  // If config.json not provided, required config values must be passed via environment variables
   const externalLaunchConfig = Object.assign(
     {},
-    ...options.config.map((configFile) => JSON.parse(readFileSync(configFile).toString()))
+    ...(options.config ?? []).map((configFile) => JSON.parse(readFileSync(configFile).toString()))
   );
 
-  const config = Object.assign(defaultServiceConfiguration, externalLaunchConfig);
+  const config: ServiceConfiguration = Object.assign(defaultServiceConfiguration, externalLaunchConfig);
+
+  // Support passing required config values via environment variables
+  config.EthereumEndpoint = process.env.ETHEREUM_ENDPOINT ?? config.EthereumEndpoint;
+  config['node-address'] = process.env.NODE_ADDRESS ?? config['node-address'];
 
   config.ExternalLaunchConfig = externalLaunchConfig;
 
